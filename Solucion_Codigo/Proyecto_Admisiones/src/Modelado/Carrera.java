4fec2c31ca599deec24f680e4b630cc25985b829
Diego-armando1 <guamangualandiegoarmando@gmail.com>
2025-05-25 17:40:40 -0500

    Segundo commit

--- Solucion_Codigo/Proyecto_Admisiones/src/Modelado/Carrera.java
+++ Solucion_Codigo/Proyecto_Admisiones/src/Modelado/Carrera.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Modelado;
 
-/**
- *
- * @author hp
- */
+package Modelado;
+import java.util.ArrayList;
+import java.util.stream.*;
+import java.util.Comparator;
 public class Carrera {
+    private String nombre;
+    private String formaAdmision;
+    private double puntajeMinimo;
+    private double puntajeNivelacion;
+    private int cupos;
+    private ArrayList<Postulante> postulantes;
+    private ArrayList<Postulante> admitidos;
+
+    public Carrera(String nombre, String formaAdmision, double puntajeMinimo, double puntajeNivelacion, int cupos) {
+        this.nombre = nombre;
+        this.formaAdmision = formaAdmision;
+        this.puntajeMinimo = puntajeMinimo;
+        this.puntajeNivelacion = puntajeNivelacion;
+        this.cupos = cupos;
+    }
+    public void agregarPostulante(Postulante postulante){
+        postulantes.add(postulante);
+    }
+    public void validarAdmision(){
+        if (this.formaAdmision.equalsIgnoreCase("Examen Admision")){
+            this.postulantes.stream()
+            .filter(p -> p.getPuntajeTotal() > this.puntajeMinimo)
+            .sorted(Comparator.comparingDouble(Postulante :: getPuntajeTotal).reversed())
+            .limit(cupos)
+            .forEach(admitidos :: add);    
+    }else if (this.formaAdmision.equalsIgnoreCase("Diagnostico")) {
+        postulantes.forEach(admitidos :: add);
+    }
+    }
+    public void procesarAdmsiones(){
+        validarAdmision();
+    }
+    public boolean mitadDeCupos(){
+        return postulantes.size() < (this.cupos / 2.0);
+    }
     
 }
--- Solucion_Codigo/Proyecto_Admisiones/src/Modelado/Postulante.java
+++ Solucion_Codigo/Proyecto_Admisiones/src/Modelado/Postulante.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Modelado;
 
-/**
- *
- * @author hp
- */
+package Modelado;
 public class Postulante {
-    
+    private String nombre;
+    private long cedula;
+    private Carrera carreraDeseada;
+    private double puntajeExamen;
+    private boolean nesecitaNivelacion;
+    private int puntajeMerito;
+    private String tipoMerito;
+    public Postulante(String nombre, long cedula, Carrera carreraDeseada){
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.carreraDeseada = carreraDeseada;
+    }
+
+    public void setPuntajeExamen(double puntajeExamen) {
+        this.puntajeExamen = puntajeExamen;
+    }
+
+    public void setTipoMerito(String tipoMerito) {
+        this.tipoMerito = tipoMerito;
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public Carrera getCarreraDeseada() {
+        return carreraDeseada;
+    }
+
+    public double getPuntajeExamen() {
+        return puntajeExamen;
+    }
+    public double getPuntajeTotal(){
+        return calcularPuntajeTotal();
+    }
+    public double calcularMeritos(){
+        if (this.tipoMerito == null){
+            if (this.tipoMerito.equalsIgnoreCase("Abanderado")){
+                this.puntajeMerito += 5;
+            }else if (this.tipoMerito.equalsIgnoreCase("Bachillerato Afin")){
+                this.puntajeMerito += 2;
+            }else if (this.tipoMerito.equalsIgnoreCase("Capacidad especial")){
+                this.puntajeMerito += 3;
+            }
+        }
+        return this.puntajeMerito;
+    }
+    public double calcularPuntajeTotal(){
+        return this.puntajeExamen + calcularMeritos();
+    }
+    public boolean requiereNivelacion(){
+        return Carrera.getFormaAdmision;
+    }
 }
